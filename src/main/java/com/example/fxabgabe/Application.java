a05ecd7a99d32dfc5600cc2ac6442bc478d907e8
ng7211 <demy.abdulsalami@haw-hamburg.de>
2025-07-13 19:48:15 +0200

    BuLi 2040/41

--- src/main/java/com/example/fxabgabe/Application.java
+++ src/main/java/com/example/fxabgabe/Application.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("TableView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("BundesLiga 2040/41");
         stage.setScene(scene);
         stage.show();
